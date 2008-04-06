7b5b1cd5a6c865540d7dc5f0841000fb49edc9c5
Ricardo SIGNES <rjbs@cpan.org>
2008-04-06 19:41:30 +0200

    refactor analyzer-finder

--- lib/CPAN/Metabase/Gateway.pm
+++ lib/CPAN/Metabase/Gateway.pm
@@ -17,7 +17,7 @@ has analyzers => (
 
 has injector => (
   is       => 'ro',
-  # isa      => 'CPAN::Metabase::Injector',
+  isa      => 'CPAN::Metabase::Injector',
   default  => sub { CPAN::Metabase::Injector->new },
   required => 1,
 );
@@ -29,13 +29,8 @@ has injector => (
 #   eval "require $_; 1" or die for $self->analyzers;
 # }
 
-sub handle {
+sub analyzer_for {
   my ($self, $request) = @_;
-
-  # XXX Yeah, uh, in the future this won't be a hashref. -- rjbs, 2008-04-06
-  $request ||= {};
-
-  die "unknown user" unless my $user = $USER_FOR_KEY{ $request->{'auth.key'} };
 
   my $analyzer;
   CANDIDATE: for my $candidate ($self->analyzers) {
@@ -45,13 +40,23 @@ sub handle {
     }
   }
 
-  die "unknown datatype" unless $analyzer;
+  return $analyzer;
+}
+
+sub handle {
+  my ($self, $request) = @_;
+
+  # XXX Yeah, uh, in the future this won't be a hashref. -- rjbs, 2008-04-06
+  $request ||= {};
+
+  die "unknown user" unless my $user = $USER_FOR_KEY{ $request->{'auth.key'} };
+  die "unknown datatype" unless my $analyzer = $self->analyzer_for($request);
 
   $request->{guid} = Data::GUID->new->as_string;
 
   my $report = $analyzer->produce_report($request);
 
-  CPAN::Metabase::Injector->inject_report($report);
+  $self->injector->inject_report($report);
 
   return 1;
 }
