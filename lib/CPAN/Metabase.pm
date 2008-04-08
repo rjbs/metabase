4c878a05039aa0354602b10dab42863b1fd7bcf6
Ricardo SIGNES <rjbs@cpan.org>
2008-04-08 11:22:22 +0200

    switch to Module::Install

--- lib/CPAN/Metabase.pm
+++ lib/CPAN/Metabase.pm
@@ -5,11 +5,23 @@
 # copy of the License from http://dev.perl.org/licenses/
 
 package CPAN::Metabase;
+use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.001';
 $VERSION = eval $VERSION; # convert '1.23_45' to 1.2345
+
+=head1 COPYRIGHT
+
+Copyright (c) 2008 by Ricardo Signes.
+
+Licensed under the same terms as Perl itself (the "License").  You may not use
+this file except in compliance with the License.  A copy of the License was
+distributed with this file or you may obtain a copy of the License from
+http://dev.perl.org/licenses/
+
+=cut
 
 1;
 
